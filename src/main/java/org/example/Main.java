fdb0c4246f93ab7eae465f6cf34a9853d7a8eac4
Francisco <francisco.romero@cesurformacion.com>
2025-11-25 20:49:02 +0100

    Query HQL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        try(Session session = DataProvider.getSessionFactory().openSession()){
+/*            Query<Game> q = session.createQuery("from Game where year<:año",Game.class);
+            q.setParameter("año",2000);
+            List<Game> juegos = q.list();
+            for(Game g : juegos){
+                System.out.println(g);
+            }
+
+ */
+
+
+        }
+
 
     }
 }
